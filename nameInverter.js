8bb288e6d5d2cac89ef13ac95dd9f4b30d90084e
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2022-11-29 18:37:26 -0500

    Implemented and tested first two req.

--- nameInverter.js
+++ nameInverter.js
@@ -1,5 +1,14 @@
 const nameInverter = function(name) {
-  //
+  // Empty string case
+  if (name === ""){
+    return name;
+  }
+
+  // Single name case (not all whitespace)
+  // if (name.length > 0 && name.trim().length > 0){
+  //  return name;
+  // }
+  return name;
   }
 
 module.exports = nameInverter;
--- test/nameInverterTest.js
+++ test/nameInverterTest.js
@@ -4,5 +4,15 @@ const assert = chai.assert;
 const nameInverter = require('../nameInverter');
 
 describe('nameInverter', function() {
+  it('should return an empty string when passed an empty string', function() {
+    const inputName = "";
+    const expectedOutput = "";
+    assert.equal(nameInverter(inputName), expectedOutput);
+  });
 
+  it('should return a single name when passed a single name', function() {
+    const inputName = "name";
+    const expectedOutput = "name";
+    assert.equal(nameInverter(inputName), expectedOutput);
+  });
 });
